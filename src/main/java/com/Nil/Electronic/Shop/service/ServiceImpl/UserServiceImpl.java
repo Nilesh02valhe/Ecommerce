4646c4504e9e500b1c096fd41d156b991316ca88
Nileshvalhe02gmail.com <nileshvalhe02@gmail.com>
2025-03-27 00:15:18 +0530

    Ecommerce:n1

--- src/main/java/com/Nil/Electronic/Shop/service/ServiceImpl/UserServiceImpl.java
+++ src/main/java/com/Nil/Electronic/Shop/service/ServiceImpl/UserServiceImpl.java
@@ -4,26 +4,28 @@ import com.Nil.Electronic.Shop.dto.UserDto;
 import com.Nil.Electronic.Shop.entity.User;
 import com.Nil.Electronic.Shop.repository.UserRepository;
 import com.Nil.Electronic.Shop.service.UserService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
-
     @Autowired
     private UserRepository userRepository;
     @Override
     public UserDto createUser(UserDto userDto) {
         String string = UUID.randomUUID().toString();
+
         User user=dtoToEntity(userDto);      // dto>Entity
         User savedUser = userRepository.save(user);
         UserDto newDto= EntityToDto (savedUser);  // Entity>Dto
         return newDto;
     }
-
     private UserDto EntityToDto(User savedUser) {
         UserDto userDto = UserDto.builder()
                 .userId(savedUser.getUserId())
@@ -36,7 +38,6 @@ public class UserServiceImpl implements UserService {
                 .build();
         return userDto;
     }
-
     private User dtoToEntity(UserDto userDto) {
         User user=User.builder()
                 .userId(userDto.getUserId())
@@ -52,33 +53,51 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDto updateUser(UserDto user, String userId) {
+    public UserDto updateUser(UserDto userDto, String userId) {
 
-        return null;
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("not fount user id"));
+        user.setName(userDto.getName());
+//        user.setEmail(userDto.getEmail());
+        user.setGender(userDto.getGender());
+        user.setImageName(userDto.getImageName());
+        user.setAbout(userDto.getAbout());
+        user.setPassword(userDto.getPassword());
+
+        User updateUser= userRepository.save(user);
+        UserDto updateDto= EntityToDto(updateUser);
+
+        return updateDto;
     }
 
     @Override
     public void deleteUser(String userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("user id is not found"));
 
     }
 
     @Override
     public List<UserDto> getAllUser() {
-        return null;
+        List<User> users = userRepository.findAll();
+        List<UserDto> dtoList = users.stream().map(user -> EntityToDto(user)).collect(Collectors.toList());
+        return dtoList;
     }
 
     @Override
     public UserDto getUserById(String userId) {
-        return null;
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User Cant find By userId");
+        return EntityToDto(user);
     }
 
     @Override
     public UserDto getUserByEmail(String email) {
-        return null;
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("user not found email And Password"));
+        return EntityToDto(user);
     }
 
     @Override
     public List<UserDto> seachUser(String keyword) {
-        return null;
+        List<User> users = userRepository.findByNameContaining(keyword);
+        List<UserDto> dtoList = users.stream().map(user -> EntityToDto(user)).collect(Collectors.toList());
+        return dtoList;
     }
 }
